4b2cd1ad099abd4fa8b787d174d4dbb1cf49f069
AndreOliveira99 <anlucasoliveira@gmail.com>
2022-05-07 22:36:28 -0300

    renamed file

--- app/getDataFromCNPJ.js
+++ app/getDataFromCNPJ.js
@@ -2,57 +2,28 @@ var axios = require('axios');
 const fs = require('fs');
 var path = require('path');
 
-const dataInput = require('../json_files/aneel-comerciais-mg.json')
-const dataInputCompaniesArray = dataInput.result.records
+const dataInput = require('../json_files/leads-Comerciais-MG-Cnpj-Only.json')
 
 let cnpjArray = []
-let cnpjCompaniesArray = []
-let cpfCompaniesArray = []
+let companyNameArray = []
 
-function getCnpjList(dataInputCompaniesArray) {
-    for (let i = 0; i < dataInputCompaniesArray.length; i++) {
-        companyObj = dataInputCompaniesArray[i]
-        if (isCpf(companyObj.NumCPFCNPJ)) {
-            cpfCompaniesArray.push(companyObj)
-        }
-        else {
-            cnpjCompaniesArray.push(companyObj)
-            cnpjArray.push(companyObj.NumCPFCNPJ)
-        }
+
+function adaptCompanyName(companyName) {
+    let newCompanyName = companyName.toString().toLowerCase()
+    newCompanyName = newCompanyName.replace(/ /g, "-")
+    return newCompanyName
+}
+
+function getCompanyNameAndArray() {
+    for (let i = 0; i < dataInput.length; i++) {
+        cnpjArray.push(dataInput[i].NumCPFCNPJ)
+        let companyName = dataInput[i].NomeTitularEmpreendimento
+        companyName = adaptCompanyName(companyName)
+        companyNameArray.push(companyName)
     }
 }
 
-function isCpf(numberString) {
-    if (numberString[0] == '*') {
-        return true
-    }
-    else {
-        return false
-    }
-}
-
-function isCompanyActive(htmlString) {
-    let htmlStringSituation = htmlString.slice(htmlString.indexOf('<p>Situação') + 12, htmlString.indexOf('<p>Data Situação Cadastral:'))
-    let companySituation = htmlStringSituation.slice(htmlStringSituation.indexOf('>') + 1, htmlStringSituation.indexOf('</b>'))
-    console.log(companySituation)
-    if (verifyStringLength) {
-        return companySituation
-    }
-    else {
-        return 'não definido'
-    }
-}
-
-getCnpjList(dataInputCompaniesArray)
-
-/*
-function getCompanyEmail(htmlString) { // O site apresenta uma forma de proteger esse dado. Avaliar uso do pupeteer
-    let htmlStringEmail = htmlString.slice(htmlString.indexOf('<p>Situação') + 12, htmlString.indexOf('<p>Data Situação Cadastral:'))
-    let companyEmail = htmlStringEmail.slice(htmlStringEmail.indexOf('>') + 1, htmlStringEmail.indexOf('</b>'))
-    console.log(companyEmail)
-    return companyEmail
-}
-*/
+getCompanyNameAndArray()
 
 function verifyStringLength(string) {
     if (string.length < 25) {
@@ -63,9 +34,33 @@ function verifyStringLength(string) {
     }
 }
 
+function isCompanyActive(htmlString) {
+    let htmlStringSituation = htmlString.slice(htmlString.indexOf('<li>Situação:') + 13, htmlString.indexOf('Natureza'))
+    let companySituation = htmlStringSituation.slice(htmlStringSituation.indexOf('<strong>') + 8, htmlStringSituation.indexOf('</strong>'))
+    console.log(companySituation)
+    if (verifyStringLength) {
+        return companySituation
+    }
+    else {
+        return 'não definido'
+    }
+}
+
+function getCompanyEmail(htmlString) {
+    let htmlStringEmail = htmlString.slice(htmlString.indexOf('<li>E-mail:') + 11, htmlString.indexOf('<strong>Quadro'))
+    let companyEmail = htmlStringEmail.slice(htmlStringEmail.indexOf('<strong>') + 8, htmlStringEmail.indexOf('</strong>'))
+    console.log(companyEmail)
+    if (verifyStringLength) {
+        return companyEmail
+    }
+    else {
+        return 'não definido'
+    }
+}
+
 function getCompanyCapital(htmlString) {
-    let htmlStringCapital = htmlString.slice(htmlString.indexOf('<p>Capital') + 10, htmlString.indexOf('<p>Tipo'))
-    let companyCapital = htmlStringCapital.slice(htmlStringCapital.indexOf('"copy">R$') + 10, htmlStringCapital.indexOf('</b>'))
+    let htmlStringCapital = htmlString.slice(htmlString.indexOf('<li>Capital Social:') + 19, htmlString.indexOf('data-slot="4"'))
+    let companyCapital = htmlStringCapital.slice(htmlStringCapital.indexOf('<strong>') + 8, htmlStringCapital.indexOf('</strong></li>'))
     console.log(companyCapital)
     if (verifyStringLength) {
         return companyCapital
@@ -76,8 +71,8 @@ function getCompanyCapital(htmlString) {
 }
 
 function getCompanyPhone(htmlString) { // Algumas empresas podem ter mais de um telefone, avaliar isso.
-    let htmlStringPhone = htmlString.slice(htmlString.indexOf('<p>Telefone') + 12, htmlString.indexOf('<h2>Localiza'))
-    let companyPhone = htmlStringPhone.slice(htmlStringPhone.indexOf('href="tel:') + 11, htmlStringPhone.indexOf('> (Ligar)'))
+    let htmlStringPhone = htmlString.slice(htmlString.indexOf('<li>Telefone:') + 13, htmlString.indexOf('<li>E-mail:'))
+    let companyPhone = htmlStringPhone.slice(htmlStringPhone.indexOf('<strong>') + 8, htmlStringPhone.indexOf('</strong>'))
     console.log(companyPhone)
     if (verifyStringLength) {
         return companyPhone
@@ -88,8 +83,8 @@ function getCompanyPhone(htmlString) { // Algumas empresas podem ter mais de um 
 }
 
 function getCompanyAddress(htmlString) {
-    let htmlStringAddress = htmlString.slice(htmlString.indexOf('<p>CEP:') + 8, htmlString.indexOf('<p>Munic'))
-    let companyAddress = htmlStringAddress.slice(htmlStringAddress.indexOf('"copy">') + 8, htmlStringAddress.indexOf('</b>'))
+    let htmlStringAddress = htmlString.slice(htmlString.indexOf('<li>CEP:') + 8, htmlString.indexOf('<li>Logradouro:'))
+    let companyAddress = htmlStringAddress.slice(htmlStringAddress.indexOf('<strong>') + 8, htmlStringAddress.indexOf('</strong>'))
     console.log(companyAddress)
     if (verifyStringLength) {
         return companyAddress
@@ -103,18 +98,37 @@ function getCompanyAddress(htmlString) {
 function getCompanyDataOutput(htmlString) {
     let companyDataOutput = {}
     companyDataOutput.Situacao = isCompanyActive(htmlString)
-    //companyDataOutput.Email = getCompanyEmail(htmlString)
+    companyDataOutput.Email = getCompanyEmail(htmlString)
     companyDataOutput.Capital = getCompanyCapital(htmlString)
     companyDataOutput.Telefone = getCompanyPhone(htmlString)
     companyDataOutput.Cep = getCompanyAddress(htmlString)
     return companyDataOutput
 }
 
-makeRequest = async (cnpj) => {
+makeRequest = async (cnpj, companyName) => {
+    console.log(`https://cnpjs.rocks/cnpj/${cnpj}/${companyName}.html`)
     var config = {
         method: 'get',
-        url: `http://api.scraperapi.com?api_key=eadcafd66d6e5858bf3b6a2700e0e1d6&url=https://cnpj.biz/${cnpj}`,
+        url: `https://cnpjs.rocks/cnpj/${cnpj}/${companyName}.html`,
+        headers: {
+            'authority': 'cnpjs.rocks',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+            'cache-control': 'max-age=0',
+            'cookie': 'PHPSESSID=qslehlv977f1vdrhqum0eq5u91; _ga=GA1.2.1683108282.1651877434; _gid=GA1.2.1030094482.1651877434; _ga_70F5D0CW71=GS1.1.1651877433.1.0.1651877826.0',
+            'referer': 'https://www.google.com/',
+            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'cross-site',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36'
+        }
     }
+
     let companyDataOutput = await axios(config)
         .then(function (response) {
             console.log(`request at ${new Date().toUTCString()}`)
@@ -126,26 +140,26 @@ makeRequest = async (cnpj) => {
     return companyDataOutput
 }
 
-async function getFinalList(cnpjArray) {
+async function getFinalList(cnpjArray, companyNameArray) {
     let companyList = []
-    for (i = 0; i < 5; i++) { // i deve ser menor que cnpjArray.length para pegar todos os dados
+    for (i = 0; i < 2; i++) { // i deve ser menor que cnpjArray.length para pegar todos os dados
         if (i % 1 == 0) { // É possível mudar a quantidade de requisições em paralelo mudando o número 1 para o número de operações paralelas desejadas. 
             await new Promise(resolve => setTimeout(resolve, 500)) // Spleep artifical pra esperar 5 segundos entre as requisições.
-            companyList[i] = await makeRequest(cnpjArray[i])
-            companyList[i].Url = `https://cnpj.biz/${cnpjArray[i]}`
+            companyList[i] = await makeRequest(cnpjArray[i], companyNameArray[i])
+            companyList[i].Url = `https://cnpjs.rocks/cnpj/${cnpjArray[i]}/${companyNameArray[i]}.html`
         }
         else {
-            companyList[i] = await makeRequest(cnpjArray[i])
-            companyList[i].Url = `https://cnpj.biz/${cnpjArray[i]}`
+            companyList[i] = await makeRequest(cnpjArray[i], companyNameArray[i])
+            companyList[i].Url = `https://cnpjs.rocks/cnpj/${cnpjArray[i]}/${companyNameArray[i]}.html`
         }
         console.log(i)
     }
     return companyList
 }
 
-getFinalList(cnpjArray)
+getFinalList(cnpjArray, companyNameArray)
     .then(companyList => {
-        var filename = path.join(__dirname, '../json_files/leads-Comerciais-MG-Cnpj.json');
+        var filename = path.join(__dirname, '../json_files/leads-Comerciais-MG-Cnpj-com-email.json');
         let dataFile = JSON.stringify(companyList);
         fs.writeFileSync(filename, dataFile, (err) => {
             if (err) throw err
