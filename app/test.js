dc1eb0039d8341b5688b9dcc39c094ff9fba03a1
AndreOliveira99 <anlucasoliveira@gmail.com>
2022-05-07 22:37:51 -0300

    deleted test.js file

--- app/test.js
+++
@@ -1,8 +0,0 @@
-function adaptCompanyName (companyName) {
-    companyName = companyName.toString().toLowerCase()
-    console.log(companyName)
-    companyName = companyName.replace(/ /g,"-")
-    return companyName
-}
-
-console.log(adaptCompanyName ('AAAA SDSA ASAA'))
